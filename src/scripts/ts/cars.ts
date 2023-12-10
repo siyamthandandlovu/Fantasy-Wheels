2d80fc766617b7908134aabfea364e9096934361
Siyamthanda Ndlovu <siyamthanda.ndlovu.gasa@gmail.com>
2023-12-10 18:23:31 +0200

    Delete cars.ts

--- src/scripts/ts/cars.ts
+++
@@ -1,6 +0,0 @@
-console.log('Welcome to Fantasy Wheels ^_^');
-
-//get json file in question
-//import jsonCars from '../../json/cars.json';
-const jsonCars = JSON.parse(require('fs').readFileSync('../../json/cars.json', 'utf8'));
-console.log(jsonCars);
